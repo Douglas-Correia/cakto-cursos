3377da79b9ac826764cf4412b3dd4bf916e835e4
Douglas-Correia <douglasmestre6@gmail.com>
2024-10-31 02:39:30 -0400

    Ajustado paginas

--- src/features/course/hooks/UseVideoQuality.tsx
+++ src/features/course/hooks/UseVideoQuality.tsx
@@ -1,34 +1,34 @@
-import { useCaktoPlayer } from '@/features/course/contexts/VideoPlayerContext';
+// import { useCaktoPlayer } from '@/features/course/contexts/VideoPlayerContext';
 
-type VideoQuality = {
-  label: string;
-  hd: boolean;
-};
+// type VideoQuality = {
+//   label: string;
+//   hd: boolean;
+// };
 
-const useVideoQuality = () => {
-  const { player } = useCaktoPlayer();
+// const useVideoQuality = () => {
+//   const { player } = useCaktoPlayer();
 
-  const internalPlayer = player.current?.getInternalPlayer?.('hls');
+//   const internalPlayer = player.current?.getInternalPlayer?.('hls');
 
-  const qualities = (internalPlayer?.levels?.map((level: { height: number }) => ({
-    label: `${level.height}p`,
-    hd: level.height >= 720,
-  })) || []) as VideoQuality[];
+//   const qualities = (internalPlayer?.levels?.map((level: { height: number }) => ({
+//     label: `${level.height}p`,
+//     hd: level.height >= 720,
+//   })) || []) as VideoQuality[];
 
-  const changeQuality = (index: number) => {
-    if (!internalPlayer) {
-      return;
-    }
+//   const changeQuality = (index: number) => {
+//     if (!internalPlayer) {
+//       return;
+//     }
 
-    internalPlayer.currentLevel = index;
-    internalPlayer!._media.play();
-  };
+//     internalPlayer.currentLevel = index;
+//     internalPlayer!._media.play();
+//   };
 
-  return {
-    changeQuality,
-    quality: internalPlayer?.currentLevel || 0,
-    qualities,
-  };
-};
+//   return {
+//     changeQuality,
+//     quality: internalPlayer?.currentLevel || 0,
+//     qualities,
+//   };
+// };
 
-export { useVideoQuality };
+// export { useVideoQuality };
